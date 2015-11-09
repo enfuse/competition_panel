d7288f91b9d4300a1669467daf4a1a3375ea5cee
Juanjo <juanjo.lopez@biko2.com>
2015-11-09 16:52:20 +0100

    Retoco app go para que los pasos sean de 5 en 5 y reaccionen al key release

--- client/app/go/go.controller.js
+++ client/app/go/go.controller.js
@@ -1,6 +1,40 @@
 'use strict';
 
 angular.module('counterApp')
-  .controller('GoCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('GoCtrl', function ($scope, $http) {
+    $scope.activeParticipantes = [];
+    var handler = function(e){
+      console.log("as");
+      switch(e.keyCode){
+        case 37:
+          $scope.activeParticipantes[0].points=$scope.activeParticipantes[0].points+5;
+          break;
+        case 39:
+          $scope.activeParticipantes[1].points=$scope.activeParticipantes[1].points+5;;
+          break;
+        /*case 51:
+          $scope.activeParticipantes[2].points++;
+          break;
+        case 52:
+          $scope.activeParticipantes[3].points++;
+          break;*/
+      }
+
+      $scope.$apply();
+
+    };
+
+    var $doc = angular.element(document);
+
+    $doc.on('keyup', handler);
+    $scope.$on('$destroy',function(){
+      $doc.off('keyup', handler);
+    })
+    $http.get('/api/participantes/activos').success(function(activos) {;
+      angular.forEach(activos, function(participante, key) {
+        participante.points = 0;
+        $scope.activeParticipantes.push(participante);
+      });
+
+    });
   });
